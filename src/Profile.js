e5e10764f1b6c6cad83be534a4af5878118a46be
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-05 11:02:25 +0530

    🚮  cleanup: componentWillUnmount componentDidUpdate

--- src/Profile.js
+++ src/Profile.js
@@ -19,6 +19,17 @@ export class Profile extends React.Component {
     this.loadUserData();
   }
 
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+
   render() {
     const isLoading = this.state.userData === null ? true : false;
 
